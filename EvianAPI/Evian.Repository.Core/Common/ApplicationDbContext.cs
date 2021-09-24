7fcab7cf46820d585e5f83bcae2ee91a448d8ebc
Eduardo Antonio Fraga Chiamulera <eduardo.chiamulera@linx.com.br>
2021-09-24 19:17:46 -0300

    contexto banco e interface repositorio

---
+++ EvianAPI/Evian.Repository.Core/Common/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evian.Repository.Core.Common
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
