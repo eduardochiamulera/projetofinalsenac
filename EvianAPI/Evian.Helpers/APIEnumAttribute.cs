0a7063bf94b338fdd7de769460efc053bd99ac98
Eduardo Antonio Fraga Chiamulera <eduardo.chiamulera@linx.com.br>
2021-10-01 18:07:56 -0300

    adicionado classes de helpers

---
+++ EvianAPI/Evian.Helpers/APIEnumAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace Evian.Helpers
+{
+    public class APIEnumAttribute : System.Attribute
+    {
+        private string EnumContext { get; set; }
+
+        public APIEnumAttribute(string APIPropertyName)
+        {
+            EnumContext = APIPropertyName;
+        }
+
+        public string Get(string appName)
+        {
+            return string.Concat(appName, EnumContext);
+        }
+    }
+}
---
+++ EvianAPI/Evian.Helpers/ResultBase.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evian.Helpers
+{
+    public class ResultBase<T>
+    {
+        [JsonProperty("total")]
+        public int Total { get; set; }
+
+        [JsonProperty("data")]
+        public List<T> Data { get; set; }
+
+        public bool HasNext
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(_NextLink);
+            }
+        }
+
+        [JsonProperty("@odata.context")]
+        public string _Context { get; set; }
+
+        [JsonProperty("@odata.nextLink")]
+        public string _NextLink { get; set; }
+    }
+}
